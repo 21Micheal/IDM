eb04279a0af7013410c7dfcf9c3e85bb08e5bdfe
Michael <michaelanjelo61@gmail.com>
2026-05-07 14:27:24 +0300

    Add remote access to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -71,6 +71,7 @@ export default defineConfig({
       "127.0.0.1",
       ".ngrok-free.dev",
       "superaesthetically-semicylindric-gidget.ngrok-free.dev",
+      "sb-726r3ftdtyrb.vercel.run",
     ],
     historyApiFallback: true,
     proxy: {
